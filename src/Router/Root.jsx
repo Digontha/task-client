09e7fdb48f71f723d13e7e50d8a45c9c794ea35c
Digontha <digonthodj@gmail.com>
2023-12-21 00:28:16 +0600

    create pages like home about contact etc

--- src/Router/Root.jsx
+++ src/Router/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div>
+        <div className='bg-[#DDD8DE] min-h-screen'>
            <Outlet></Outlet>
         </div>
     );
