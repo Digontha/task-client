1c7c096c3c651c359d0c6f89caf670ca06eceed5
Digontha <digonthodj@gmail.com>
2023-12-21 01:34:29 +0600

    create banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import Navbar from '../Shared/Navbar';
+import banner from "../assets/document-specification-isometric-design-vector-removebg.png"
 
 const Home = () => {
     return (
-        <div >
-            
-            Home
+        <div className="flex flex-col lg:flex-row items-center justify-between px-10">
+
+            <div className="space-y-10">
+                <a className="font-mono text-white hidden lg:block text-5xl ">TO<span className='text-7xl  text-orange-900 ml-5'>DO</span></a>
+                <p className="lg:text-5xl text-[#3D2342] lg:w-[800px] font-mono">Empowering you to conquer your day with ease. Your productivity, Reimagined.</p>
+                <div>
+                    <button className="btn btn-outline text-[#A955BA] font-semibold lg:text-3xl">Let’s Explore</button>
+                </div>
+            </div>
+
+            <div>
+                <img src={banner} alt="" />
+            </div>
+
         </div>
     );
 };
