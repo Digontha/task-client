002666bd01fd356d07a02d2c998d5415d38ec5cf
Digontha <digonthodj@gmail.com>
2023-12-21 00:43:23 +0600

    create navbar

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -2,21 +2,21 @@
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-    const navLinks = <>
+    const navLinks = <div className='text-[20px] flex gap-5 items-center font-semibold '>
         <NavLink  to="/" className={({ isActive, isPending }) =>
        
-            isPending ? "pending" : isActive ? "text-red-700" : "text-black"
+            isPending ? "pending" : isActive ? "text-white bg-[#A955BA] p-[5px] rounded-xl" : "text-black"
         }>Home
         </NavLink>
         <NavLink  to="/about" className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-red-700" : "text-black"
+            isPending ? "pending" : isActive ? "text-white bg-[#A955BA] p-1 rounded-xl" : "text-black"
         }>About
         </NavLink>
         <NavLink  to="/contact" className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "text-red-700" : "text-black"
+            isPending ? "pending" : isActive ? "text-white bg-[#A955BA] p-1 rounded-xl" : "text-black"
         }>Contact
         </NavLink>
-    </>
+    </div>
     return (
         <>
             <div className="navbar bg-[#AE8FB4] p-5">
@@ -29,16 +29,16 @@ const Navbar = () => {
                            {navLinks}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost font-mono text-white text-xl">TO<span className='text-3xl  text-orange-900'>DO</span></a>
                 </div>
-                <div className="navbar-center hidden lg:flex">
+                <div className="navbar-end hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                     {navLinks}
                     </ul>
                 </div>
-                <div className="navbar-end">
+                {/* <div className="navbar-end">
                     <a className="btn">Button</a>
-                </div>
+                </div> */}
             </div>
         </>
     );
