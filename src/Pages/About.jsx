05feffd5fa001df9b66835530025189efd252375
Digontha <digonthodj@gmail.com>
2023-12-21 23:50:13 +0600

    create about and contact page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
-
+import { GoPackageDependencies } from "react-icons/go";
+import { ImEnter, ImLocation } from "react-icons/im";
+import { PiAnchorSimpleDuotone } from "react-icons/pi";
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <>
+            <div className='text-center my-10 text-4xl font-mono underline' >
+                <h1 >
+                    About Us
+                </h1>
+            </div>
+
+            <div>
+                <p className='flex items-center justify-center text-3xl font-mono'>
+                    Welcome to Your <a className="font-mono text-white hidden md:block text-5xl ml-5 ">TO<span className='text-7xl  text-orange-900 ml-5'>DO</span></a>
+                </p>
+            </div>
+
+            <div className='text-center my-10 text-4xl font-mono underline' >
+                <h1 >
+                    Our Mission
+                </h1>
+            </div>
+            <div className='lg:w-2/3 mx-auto border border-3 border-black rounded-full p-14'>
+                <p className='lg:text-3xl'>At ,TO<span className='text-4xl text-orange-900 '>DO</span> , our mission is to simplify your life by providing a robust and intuitive task management solution. We understand the challenges of modern life, where tasks and responsibilities can quickly pile up. Our goal is to empower individuals and teams to stay organized, boost productivity, and achieve their goals with ease.</p>
+            </div>
+
+            <div className='text-center my-10 text-4xl font-mono underline' >
+                <h1 >
+                    Our Core Values
+                </h1>
+            </div>
+
+            <div className="lg:text-3xl flex justify-center items-center lg:gap-10">
+                <p className="flex items-center gap-3 border-3 border p-1 border-black"><PiAnchorSimpleDuotone></PiAnchorSimpleDuotone>Simplicity</p>
+                <p className="flex items-center gap-3 border-3 border p-1 border-black"><ImEnter></ImEnter>Innovation</p>
+                <p className="flex items-center gap-3 border-3 border p-1 border-black"><GoPackageDependencies />Reliability</p>
+            </div>
+
+            
+            <div className='text-center my-10 text-4xl font-mono underline' >
+                <h1 >
+                    Our Location
+                </h1>
+            </div>
+
+            <div>
+                <p className="lg:flex justify-center gap-3 items-center lg:text-3xl font-mono"><ImLocation></ImLocation> Bangladesh,Dhaka,Uttara-22</p>
+            </div>
+        </>
     );
 };
 
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,9 +2,37 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-           contact 
-        </div>
+        <>
+        <div className="flex items-center justify-center h-[30vh]">
+                <h1 className='text-3xl text-center font-mono font-semibold underline text-black'>Contact us</h1>
+            </div>
+
+            <section className="max-w-3xl mx-auto mt-8 p-6 bg-[#22252F] text-white rounded-md shadow-md">
+        
+        <p className="text-white text-2xl font-mono mb-6">Let's Connect And Work</p>
+
+        <form  >
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="mb-4">
+                    <label className="block text-sm font-semibold text-white">Name</label>
+                    <input type="text" id="name" name="from_name" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-400" required/>
+                </div>
+
+                <div className="mb-4">
+                    <label  className="block text-sm font-semibold text-white">Email</label>
+                    <input type="email" id="email" name="from_email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-400" required/>
+                </div>
+            </div>
+
+            <div className="mb-4">
+                <label  className="block text-sm font-semibold text-white">Message</label>
+                <textarea id="message" name="message" rows="4" className="w-full px-4 py-2 border rounded-md focus:outline-none text-black focus:border-blue-400" required></textarea>
+            </div>
+
+            <button type="submit" className="bg-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300">Submit</button>
+        </form>
+    </section>
+        </>
     );
 };
 
